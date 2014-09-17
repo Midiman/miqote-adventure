242eb69639cc0654e5a95c40be8858a826c5341b
Jonathan Payne <potentj@gmail.com>
2014-09-17 15:49:55 -0700

    Place-Holder music

---
+++ Themes/miqote-adventure/Scripts/02 Colors.lua
@@ -0,0 +1,10 @@
+ThemeColor = {
+	Text			= color("#ffffff"),
+	TextDark		= color("#e6e9ed"),
+	Primary			= color("#ed5565"),
+	PrimaryDark		= color("#da4453"),
+	Secondary		= color("#ffbb33"),
+	SecondaryDark	= color("#ff8800"),
+	Background		= color("#656d78"),
+	BackgroundDark	= color("#434a54"),
+}
